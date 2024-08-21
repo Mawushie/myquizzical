5f95750504aff930837878e2834721be6ad53b9e
Mawushie <afi.mawushie.honyah@gmail.com>
2024-08-21 02:05:54 +0000

    feature to get selected answer and correct answer started

---
+++ src/components/Question.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import he from "he";
+export default function Question({ quiz, getSelectedAnswer }) {
+  const { question, allAnswers, correct_answer } = quiz;
+  // console.log(quiz);
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+  let randomizedAnswers = shuffleArray(allAnswers);
+  const answerElements = randomizedAnswers.map((answer, index) => {
+    return (
+      <p
+        className="border border-myborder text-myborder rounded-lg py-1 px-4 text-base font-bold cursor-pointer"
+        key={index}
+        onClick={() => getSelectedAnswer(he.decode(answer), correct_answer)}
+      >
+        {he.decode(answer)}
+      </p>
+    );
+  });
+  return (
+    <div className="flex flex-col items-start border-b">
+      <div className="mb-3 pt-3">
+        <p className="text-myborder font-bold text-lg ">{question}</p>
+      </div>
+      <div className="flex gap-3 mb-3 pb-2">{answerElements}</div>
+    </div>
+  );
+}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,37 +1,64 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import Question from "./Question";
+import he from "he";
 
 export default function Quiz({ toggleStart, categoryId }) {
+  const [quizData, setQuizData] = useState([]);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
   // console.log(categoryId);
-  if (categoryId === " ") {
-    console.log("any category was selected");
-  }
+  // if (categoryId === " ") {
+  //   console.log("any category was selected");
+  // }
+
+  const getSelectedAnswer = (selectedAnswer, correctAnswer) => {
+    console.log(selectedAnswer);
+    console.log(correctAnswer);
+  };
 
   useEffect(() => {
     let fetchUrl =
       categoryId === " "
         ? `https://opentdb.com/api.php?amount=5`
         : `https://opentdb.com/api.php?amount=5&category=${categoryId}`;
-    console.log(fetchUrl);
+    // console.log(fetchUrl);
     fetch(`${fetchUrl}`)
       .then((res) => {
         if (res.status !== 200) {
           console.log(res.status);
-          console.log("response not okay", res);
+          console.log(res);
           throw "Something went wrong";
         }
         return res.json();
       })
-      .then((data) => console.log(data))
+      .then((data) => {
+        console.log(data.results);
+        let quizArray = data.results.map((quiz) => {
+          const { question, incorrect_answers, correct_answer } = quiz;
+          return {
+            question: he.decode(question),
+            allAnswers: [...incorrect_answers, correct_answer],
+            correct_answer: correct_answer,
+          };
+        });
+        setQuizData(quizArray);
+        console.log(quizArray);
+      })
       .catch((err) => {
-        console.log(err, "there was an error");
+        console.log(err);
       });
   }, []);
+
+  const quizDisplayElements = quizData.map((quiz, index) => {
+    return (
+      <Question quiz={quiz} key={index} getSelectedAnswer={getSelectedAnswer} />
+    );
+  });
   return (
-    <div className="flex">
+    <div className="flex flex-col">
+      {quizDisplayElements}
       <button onClick={toggleStart} className="border bg-btn text-white">
         Play again
       </button>
-      Quiz Display page
     </div>
   );
 }
